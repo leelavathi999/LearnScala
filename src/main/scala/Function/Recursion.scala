b04b116068ef6ab8128bf46ea71bb64befb1a03e
Dande Leelavathi <leelavathid999@gmail.com>
2025-01-27 14:58:00 +0530

    initial commit

--- src/main/scala/Function/Recursion.scala
+++ src/main/scala/Function/Recursion.scala
@@ -1,3 +1,14 @@
 package Function
 
-object Recursion
+import scala.jdk.Accumulator
+
+object Recursion extends  App{
+  def factorial(n:Int):BigInt= {
+    def factHelper(x: Int, accumulator: BigInt): BigInt =
+      if (x <= 1) accumulator
+      else factHelper(x - 1, x * accumulator)
+
+    factHelper(n, 1)
+  }
+  println(factorial(10))
+}
